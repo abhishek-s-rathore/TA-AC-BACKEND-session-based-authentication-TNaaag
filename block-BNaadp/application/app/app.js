08a6085787d53618cd9060d2299074841c602d0b
Abhishek <abhishek.990914@gmail.com>
2021-09-02 00:50:11 +0530

    refectored

--- block-BNaadp/application/app/app.js
+++ block-BNaadp/application/app/app.js
@@ -14,28 +14,40 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Using Middelwares
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true,
+  })
+);
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Cookie
+
+app.use((req, res, next) => {
+  console.log(req.cookies);
+  res.cookie('name', 'Abhishek');
+  next();
+});
+
+// Using Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
