5cfb403ee124ee487c900e3cc4ff3c282a60fc55
Abhishek <abhishek.990914@gmail.com>
2021-09-03 01:40:57 +0530

    block Completed

--- block-BNaaey/application/session-demo/app.js
+++ block-BNaaey/application/session-demo/app.js
@@ -4,9 +4,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+// Connecting To Database
+mongoose.connect(
+  'mongodb://localhost/sessionDemo',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    console.log('Connected To Database: ', err ? false : true);
+  }
+);
 
 var app = express();
 
@@ -18,24 +31,36 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true,
+  })
+);
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    // store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/sessionDemo' }),
+  })
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
